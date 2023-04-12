058bfd463340677f696d7d9884cb9424c3a3d896
Rahul Sharma <rahulsh3105@gmail.com>
2023-04-12 12:24:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+var currentSound;
+
+function playSound(sound) {
+	if(currentSound) {
+		currentSound.pause();
+		currentSound.currentTime = 0;
+	}
+
+	currentSound = new Audio('sounds/' + sound);
+	currentSound.play();
+}
+
+function stopSound() {
+	if(currentSound) {
+		currentSound.pause();
+		currentSound.currentTime = 0;
+	}
+}
